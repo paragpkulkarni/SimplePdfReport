c5530e2d9f6c07e0985fea6a096dd33efa8cedb9
Parag Kulkarni <parag.kulkarni@nitorinfotech.com>
2020-05-05 22:34:12 +0530

    made changes to generate large files

--- src/SimplePdfReport.Reporting.MigraDoc/ReportPdf.cs
+++ src/SimplePdfReport.Reporting.MigraDoc/ReportPdf.cs
@@ -1,6 +1,9 @@
 ﻿using MigraDoc.DocumentObjectModel;
 using MigraDoc.Rendering;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
 using SimplePdfReport.Reporting.MigraDoc.Internal;
+using System.IO;
 
 namespace SimplePdfReport.Reporting.MigraDoc
 {
@@ -68,6 +71,38 @@ namespace SimplePdfReport.Reporting.MigraDoc
         private void AddStructureSet(Section section, StructureSet structureSet)
         {
             new StructureSetContent().Add(section, structureSet);
+        }
+
+        public static void CombinePDFs(string path, string outputFileName)
+        {
+            // Get some file names
+            string[] files = System.IO.Directory.GetFiles(path, "*.pdf");
+
+            // Open the output document
+            PdfDocument outputDocument = new PdfDocument();
+
+            // Iterate files
+            foreach (string file in files)
+            {
+                // Open the document to import pages from it.
+                PdfDocument inputDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+
+                // Iterate pages
+                int count = inputDocument.PageCount;
+                for (int idx = 0; idx < count; idx++)
+                {
+                    // Get the page from the external document...
+                    PdfPage page = inputDocument.Pages[idx];
+                    // ...and add it to the output document.
+                    outputDocument.AddPage(page);
+                }
+            }
+
+            // Save the document...
+            string filename = Path.Combine(path,outputFileName);
+            outputDocument.Save(filename);
+            // ...and start a viewer.
+            
         }
     }
 }
